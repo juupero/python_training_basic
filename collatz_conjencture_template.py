e358f23a9c4d91a2c038ef3837c454e63a41d25a
Jussi Ojala <jussi.ojala@midaxo.com>
2019-03-07 09:05:06 +0200

    Half done

--- collatz_conjencture_template.py
+++ collatz_conjencture_template.py
@@ -1,21 +1,27 @@
 def get_cycle_length(n):
-    """ 
-    takes an integer 0 < n < 10000
-    returns the number of steps it
-    will take to get to 1
-    by performing n // 2 if n is even
-    and n * 3 + 1 if n is odd
-    int -> int
-    print(get_cycle_length(5)) -> 6
-    print(get_cycle_length(1)) -> 1
-    print(get_cycle_length(197)) -> 27
-    """
-    # remove this pass statement and write the body of your function
-    pass
+  """ 
+  takes an integer 0 < n < 10000
+  returns the number of steps it
+  will take to get to 1
+  by performing n // 2 if n is even
+  and n * 3 + 1 if n is odd
+  int -> int
+  print(get_cycle_length(5)) -> 6
+  print(get_cycle_length(1)) -> 1
+  print(get_cycle_length(197)) -> 27
+  """
+  number_of_steps = 0
+  while n > 1:
+    if n % 2 == 0:
+      n = n // 2
+    else:
+      n = n * 3 + 1
+    number_of_steps += 1
+  return number_of_steps
 
 
 def get_cycle_length_range(start, end):
-    """ 
+    """
     takes 2 integers 0 < start, end < 10000
     finds the number for which it takes
     the maximum amount of steps to get to
@@ -26,5 +32,13 @@ def get_cycle_length_range(start, end):
     print(get_cycle_length_range(200, 100)) -> 125
     print(get_cycle_length_range(1, 2)) -> 2
     """
-    # remove this pass statement and write the body of your function
-    pass
+    # I don't understand the task assignment, yet..
+
+# Results
+print(get_cycle_length(5)) # 6
+print(get_cycle_length(1)) # 1
+print(get_cycle_length(197)) # 27
+# The results of the script are off by minus one, but if you think about it how many steps does it really take for get_cycle_length(1)? The correct answer is zero not one.
+# print(get_cycle_length_range(5, 100)) # 119
+# print(get_cycle_length_range(200, 100)) # 125
+# print(get_cycle_length_range(1, 2)) # 2
